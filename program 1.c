5e1952cc90944f217bc8d1beaf2121f8b3fc7223
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:05:15 +0530

    Update program 1.c

--- program 1.c
+++ program 1.c
@@ -21,3 +21,43 @@ int main(){
 }
 
 3)
+#include <stdio.h>
+#define SIZE 5
+int queue[SIZE], front = -1,rear = -1;
+void enqueue(int x){
+    if (rear== SIZE -1)
+    printf("Queue FULL\n");
+   else {
+       if (front == -1)
+           front = 0;
+       queue[++rear] = x;
+   }
+}
+void display() {
+    int i;
+    for (i= front; i <= rear; i++)
+    {
+        printf("P%d ",queue[i]);
+    }
+}
+ int main() {
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    printf("Ready Queue: ");
+    display();
+    return 0;
+}
+
+4)
+#include <stdio.h>
+int  main(){
+    int process[] = {1,2,3,4};
+    int i;
+    printf("Execution order:\n");
+    for(i=0;i<4;i++)
+       printf("Executing Process P%d\n",process[i]);
+       return 0;
+}
+
+5)
