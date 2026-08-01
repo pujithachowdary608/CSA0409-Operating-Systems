13ae7d723d2a06190e8ac948e3dbd4cdcc080330
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:46:07 +0530

    Create Experiment 11.c

---
+++ Lab Exercises/Experiment 11.c
@@ -0,0 +1,22 @@
+-> CODE :
+
+#include <stdio.h>
+#include <pthread.h>
+void *display(void *arg)
+{
+    printf("Thread is executing.\n");
+    return NULL;
+}
+int main()
+{
+    pthread_t t1;
+    pthread_create(&t1, NULL, display, NULL);
+    pthread_join(t1, NULL);
+    printf("Thread execution completed.\n");
+    return 0;
+}
+
+-> OUTPUT :
+
+Thread is executing.
+Thread execution complet
