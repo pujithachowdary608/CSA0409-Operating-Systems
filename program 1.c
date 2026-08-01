3986733d5e16f444b356f3857ff59ba640088de4
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:01:39 +0530

    Update program 1.c

--- program 1.c
+++ program 1.c
@@ -1,6 +1,23 @@
+1) 
 #include <stdio.h>
 #include <unistd.h>
 int main(){
      printf("current process ID: %d/n",getpid());
      return 0;
 }
+
+2)
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(){
+    fork();
+    printf("Process ID:%d\n",getpid());
+    fork();
+    printf("Process ID: %d\n",getpid());
+    wait(NULL);
+    wait(NULL);
+    return 0;
+}
+
+3)
