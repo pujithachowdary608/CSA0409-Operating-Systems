7c529ddf4aacc55cbe81956e43a6374d171580dc
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:14:13 +0530

    Delete program 4.c

--- program 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int  main(){
-    int process[] = {1,2,3,4};
-    int i;
-    printf("Execution order:\n");
-    for(i=0;i<4;i++)
-       printf("Executing Process P%d\n",process[i]);
-       return 0;
-}
