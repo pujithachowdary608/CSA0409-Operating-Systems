035b9bd3351fc3056e0434a3aa823fb3190c61f3
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:08:38 +0530

    Update program 1.c

--- program 1.c
+++ program 1.c
@@ -97,3 +97,40 @@ int main(){
 }
   return 0;
 }
+
+7) 
+#include<stdio.h>
+int main() {
+    int memory[5]={100,200,300,400,500};
+    int i;
+    printf("Memory Blocks:\n");
+    for(i=0;i<5;i++)
+       printf("Block %d = %dKB\n",i+1,memory[i]);
+       return 0;
+}
+
+8)
+#include <stdio.h>
+#include<pthread.h>
+void *thread1(void *arg) {
+    int i;
+    for (i=1;i<=5;i+=2)
+        printf("%d\n",i);
+        return NULL;
+}
+ void *thread2(void *arg) {
+     int i;
+     for(i=2;i<=5;i+=2)
+          printf("%d\n",i);
+    return NULL;
+}
+int main() {
+    pthread_t t1,t2;
+    pthread_create(&t1,NULL,thread1,NULL);
+    pthread_create(&t2,NULL,thread2,NULL);
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    return 0;
+}
+
+9)
