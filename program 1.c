6f3615526767e520f6b55e15d512376415dd3fb8
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-01 09:06:37 +0530

    Update program 1.c

--- program 1.c
+++ program 1.c
@@ -61,3 +61,39 @@ int  main(){
 }
 
 5)
+#include <stdio.h>
+int main() {
+    FILE *fp;
+    char str[100];
+    fp= fopen("sample.txt", "w");
+    fprintf(fp,"Hello operating system");
+    fclose(fp);
+    fp=fopen("sample.txt","r");
+    fgets(str,100,fp);
+    printf("File Content: %s",str);
+    fclose(fp);
+    return 0;
+}
+
+6)
+#include<stdio.h>
+int main(){
+    int pages[] = {1,2,3,1,4,5};
+    int frame[3] = {-1,-1,-1};
+    int i,j,k=0,flag;
+    for(i=0;i<6;i++){
+        flag=0;
+        for(j=0;j<3;j++)
+           if(frame[j]==pages[i])
+              flag=1;
+        if(flag==0){
+            frame[k]=pages[i];
+            k=(k+1)%3;
+        }
+        printf("Frames: ");
+        for(j=0;j<3;j++)
+           printf("%d", frame[j]);
+        printf("\n");
+}
+  return 0;
+}
