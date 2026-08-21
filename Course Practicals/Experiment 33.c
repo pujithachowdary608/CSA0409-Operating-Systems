de9fa720ee08fa5eaf361f8c0eb7610ac02196be
pujithachowdary608 <nallapujitha777@gmail.com>
2026-08-21 18:32:23 +0530

    Add Experiment 33 to list directory contents

---
+++ Course Practicals/Experiment 33.c
@@ -0,0 +1,28 @@
+-> CODE :
+
+#include <stdio.h>
+#include <dirent.h>
+int main()
+{
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(".");
+    if(dir == NULL)
+    {
+        printf("Cannot open directory\n");
+        return 1;
+    }
+    printf("Directory contents:\n");
+    while((entry = readdir(dir)) != NULL)
+        printf("%s\n", entry->d_name);
+    closedir(dir);
+    return 0;
+}
+
+-> OUTPUT :
+
+Directory contents:
+.
+..
+a.out
+main.ccp
